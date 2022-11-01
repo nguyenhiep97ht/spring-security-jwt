7da65f15bfc677d5db9f611dae6dd9c1a6a49e17
hiepn06 <hiepn06@gmail.com>
2022-11-01 15:36:09 +0700

    Add cors *

--- src/main/java/jwt/example/userservice/security/SecurityConfig.java
+++ src/main/java/jwt/example/userservice/security/SecurityConfig.java
@@ -14,6 +14,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
@@ -46,6 +49,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
   }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+      return source;
+    }
+  
   @Bean
   @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
